80f5303bc5e5fd760cd12bc87643e9ff0fba4f1f
Spiderironman1 <89696931+Spiderironman1@users.noreply.github.com>
2021-08-28 21:33:19 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("😎Next PAGE😎", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"⚡Choose Size and click start to get File ⚡..Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"🤘Choose Size and click start to get File 🤘...Found {(len_results)} Results For Your Request😻: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
